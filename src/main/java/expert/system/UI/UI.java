d5bef5b5e3035daae643ef229ab5e741920f0eb6
chaljara <chaljara@espol.edu.ec>
2017-02-03 03:46:28 -0500

    gui

--- src/main/java/expert/system/UI/UI.java
+++ src/main/java/expert/system/UI/UI.java
@@ -14,12 +14,20 @@ import java.awt.SystemColor;
 import javax.swing.JButton;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import javax.swing.JScrollPane;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.FlowLayout;
+import javax.swing.JTextArea;
+import javax.swing.ScrollPaneConstants;
+import javax.swing.JRadioButton;
 
 public class UI {
 
 	private JFrame frmCarAdvisorExpert;
 	private final JLabel lblNewLabel = new JLabel("New label");
 	private final JLabel lblNewLabel_1 = new JLabel("New label");
+	JPanel panelTitle = new JPanel();
 
 	/**
 	 * Launch the application.
@@ -55,28 +63,68 @@ public class UI {
 		frmCarAdvisorExpert.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frmCarAdvisorExpert.getContentPane().setLayout(null);
 		
+		JTextPane txtpnQuestions = new JTextPane();
+		txtpnQuestions.setBounds(21, 11, 102, 26);
+		frmCarAdvisorExpert.getContentPane().add(txtpnQuestions);
+		txtpnQuestions.setBackground(SystemColor.control);
+		txtpnQuestions.setFont(new Font("Cambria", Font.BOLD, 18));
+		txtpnQuestions.setText("QUESTIONS");
+		
+		JPanel panelQuestions = new JPanel();
+		panelQuestions.setBounds(0, 46, 784, 516);
+		frmCarAdvisorExpert.getContentPane().add(panelQuestions);
+		panelQuestions.setLayout(new GridLayout(20, 9, 0, 2));
+		
+		JTextPane txtpnYouFind = new JTextPane();
+		txtpnYouFind.setName("Q1");
+		txtpnYouFind.setEditable(false);
+		panelQuestions.add(txtpnYouFind);
+		txtpnYouFind.setFont(new Font("Cambria", Font.PLAIN, 16));
+		txtpnYouFind.setText("1. You find it difficult to introduce yourself to other people");
+		
+		JTextPane txtpnYouOften = new JTextPane();
+		txtpnYouOften.setFont(new Font("Cambria", Font.PLAIN, 16));
+		txtpnYouOften.setText("2. You often get so lost in thoughts that you ignore or forget your surroundings");
+		panelQuestions.add(txtpnYouOften);
+		
+		JTextPane txtpnYouTry = new JTextPane();
+		txtpnYouTry.setFont(new Font("Cambria", Font.PLAIN, 16));
+		txtpnYouTry.setText("3. You try to respond to your e-mails as soon as possible and cannot stand a messy inbox");
+		panelQuestions.add(txtpnYouTry);
+		
+		JTextPane txtpnYouFind_1 = new JTextPane();
+		txtpnYouFind_1.setFont(new Font("Cambria", Font.PLAIN, 16));
+		txtpnYouFind_1.setText("4. You find it easy to stay relaxed and focused even when there is some pressure");
+		panelQuestions.add(txtpnYouFind_1);
+		
+		panelTitle = new JPanel();
+		panelTitle.setVisible(false);
+		panelTitle.setBounds(199, 190, 420, 196);
+		frmCarAdvisorExpert.getContentPane().add(panelTitle);
+		panelTitle.setLayout(null);
+		
 		JLabel lblCarAdvisorExpert = new JLabel("CAR ADVISOR EXPERT SYSTEM");
-		lblCarAdvisorExpert.setFont(new Font("Tahoma", Font.PLAIN, 26));
-		lblCarAdvisorExpert.setBounds(224, 49, 363, 32);
-		frmCarAdvisorExpert.getContentPane().add(lblCarAdvisorExpert);
+		lblCarAdvisorExpert.setBounds(6, 5, 408, 32);
+		panelTitle.add(lblCarAdvisorExpert);
+		lblCarAdvisorExpert.setFont(new Font("Tahoma", Font.BOLD, 26));
 		
 		JTextPane txtpnAnExpertSystem = new JTextPane();
+		txtpnAnExpertSystem.setBounds(25, 42, 370, 56);
+		panelTitle.add(txtpnAnExpertSystem);
 		txtpnAnExpertSystem.setEditable(false);
 		txtpnAnExpertSystem.setBackground(SystemColor.control);
 		txtpnAnExpertSystem.setFont(new Font("Tahoma", Font.PLAIN, 20));
 		txtpnAnExpertSystem.setText("An expert system that advises you which\r\nvehicle to buy based on your personality");
-		txtpnAnExpertSystem.setBounds(224, 136, 378, 62);
-		frmCarAdvisorExpert.getContentPane().add(txtpnAnExpertSystem);
 		
 		JButton btnNewButton = new JButton("START");
+		btnNewButton.setBounds(168, 114, 83, 29);
+		panelTitle.add(btnNewButton);
 		btnNewButton.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent arg0) {
-				//btnNewButton.setVisible(true);
+				panelTitle.setVisible(false);
 			}
 		});
 		btnNewButton.setFont(new Font("Tahoma", Font.PLAIN, 16));
-		btnNewButton.setBounds(338, 359, 150, 50);
-		frmCarAdvisorExpert.getContentPane().add(btnNewButton);
 	}
 }
